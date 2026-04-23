6b44a7f910f9199b2383abee923313e35c37ab1f
SeyZiiZ <aitbihiyanis@gmail.com>
2026-04-24 00:11:11 +0200

    fixes

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -7,7 +7,7 @@ export KUBECONFIG=${KUBECONFIG:-/etc/rancher/k3s/k3s.yaml}
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-sudo kubectl create namespace monitoring --dry-run=client -o yaml | sudo kubectl apply -f -
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
 
 helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
   -n monitoring \
@@ -15,7 +15,7 @@ helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
   --wait --timeout 10m
 
 # Le ServiceMonitor de l'API doit porter le label release=monitoring pour être scrapé
-sudo kubectl apply -f "$(dirname "$0")/../k8s/servicemonitor.yaml"
+kubectl apply -f "$(dirname "$0")/../k8s/servicemonitor.yaml"
 
 echo ""
 echo "Grafana :     http://<IP_VM>:31000   (admin / changeme-grafana)"
